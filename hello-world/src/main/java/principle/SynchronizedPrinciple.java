4177ea054242cc6ab4bd1f5222d05e59e6017ff6
xuelin <353006063@qq.com>
2021-08-03 20:56:03 +0800

    init note

--- hello-world/src/main/java/principle/SynchronizedPrinciple.java
+++ hello-world/src/main/java/principle/SynchronizedPrinciple.java
@@ -1,21 +1,41 @@
 package principle;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Objects;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
 /**
  * @author xuelin
  */
 public class SynchronizedPrinciple {
 
-    static final Object obj = new Object();
-
-    public static void method1() {
-        synchronized (obj) { // 同步块 A
-            method2();
-        }
-    }
-
-    public static void method2() {
-        synchronized (obj) { // 同步块 B
-        }
-    }
 }
 
+class GuardedObject {
+    private final Object lock = new Object();
+    private Object response;
+
+    public Object get() {
+        synchronized (lock) {
+            // 条件不满足则等待
+            while (response == null) {
+                try {
+                    lock.wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            return response;
+        }
+    }
+
+    public void complete(Object response) {
+        synchronized (lock) {
+            // 条件满足，通知等待线程
+            this.response = response;
+            lock.notifyAll();
+        }
+    }
+
+}
